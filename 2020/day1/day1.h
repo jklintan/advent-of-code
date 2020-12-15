7e5ef8923f13b236f730a2da603aba6156d52f3c
jklintan <joskl841@student.liu.se>
2020-12-15 22:16:03 +0100

    Add day 1 solutions

---
+++ 2020/day1/day1.h
@@ -0,0 +1,66 @@
+#ifndef DAY1
+#define DAY1
+
+// Calculate the product of the sum of two terms that adds up to input specific number, if existing in input
+int productOfSum2(list<int> l, int sum) {
+
+    int goal{ sum };
+    int missing{ 0 };
+    int currentItem{ 0 };
+
+    l.sort();
+
+    for (list<int>::iterator it = l.begin(); it != l.end(); ++it) {
+        currentItem = *it;
+
+        if (currentItem > goal)
+            return 0;
+
+        missing = goal - currentItem;
+
+        auto it2 = find(next(it), l.end(), missing);
+        if (it2 != l.end()) {
+            return (*it2) * currentItem;
+        }
+    }
+
+    return 0; // No pairs found
+};
+
+// Calculate the product of the sum of three terms that adds up to input specific number, if existing in input
+int productOfSum3(list<int> l, int sum) {
+
+    int goal{ sum };
+    int missing{ 0 };
+    int currentItem{ 0 };
+    int nextItem{ 0 };
+
+    l.sort();
+
+    for (list<int>::iterator it = l.begin(); it != l.end(); ++it) {
+
+        currentItem = *it;
+        if (currentItem > goal)
+            return 0;
+
+        missing = goal - currentItem;
+
+        if (missing > 0) {
+            for (list<int>::iterator it2 = next(it); it2 != l.end(); ++it2) {
+                nextItem = *it2;
+                missing = goal - nextItem - currentItem;
+
+                if (missing < 0)
+                    break;
+
+                auto missingTerm = find(next(it2), l.end(), missing);
+                if (missingTerm != l.end())
+                    return (*missingTerm) * nextItem * currentItem;
+            }
+        }
+    }
+
+    return 0; // No triple found
+};
+
+#endif 
--- 2020/main.cpp
+++ 2020/main.cpp
@@ -1,10 +1,45 @@
 #include <iostream>
 #include <cstdlib>
+#include <list>
+#include <fstream>
+#include <filesystem>
+#include <stdio.h>
+#include <string>
 
 using namespace std;
 
+#include "day1/day1.h"
+
+list<int> testDay1{1721, 979, 366, 299, 675, 1456};
+
+list<int> processInputNumbers(string name) {
+    ifstream inputFile;
+    inputFile.open(name, ios::in);
+
+    list<int> processed;
+    int numb;
+
+    if (inputFile.is_open()) {
+        string w;
+        while (getline(inputFile, w)) {
+            processed.push_back(stoi(w));
+        }
+        inputFile.close(); //close the file object.
+    }
+
+    return processed;
+}
+
 int main(int argc, char* argv[]) {
-    cout << "Hello";
+    
+    // Day 1
+#if 0
+    list<int> inputDay1 = processInputNumbers("../../AdventOfCode/advent-of-code/2020/day1/input.txt");
+    if (inputDay1.size() != 0) {
+        cout << productOfSum2(inputDay1, 2020);
+        cout << productOfSum3(inputDay1, 2020);
+    }
+#endif
 
     return 1;
 };
